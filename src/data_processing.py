279604cf1cc91b6bc7e1ff03701c3eeb78732c68
BrayanSGL <bsgaleanol@udistrital.edu.co>
2025-01-05 16:15:25 -0500

    Agregar archivos principales y de procesamiento de datos

---
+++ src/data_processing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+class DataProcessing:
+    def __init__(self, data):
+        self.data = data
+
+    def clean_data(self):
+        # Clean data
+        return self.data
+
+    def transform_data(self):
+        # Transform data
+        return self.data
+
+    def process_data(self):
+        self.clean_data()
+        self.transform_data()
+        return self.data
