3d327e41c6bdc0f949c64facf8dc22d8922cd3b7
Jonathan Bernate <87962422+JonathanBernate@users.noreply.github.com>
2025-01-05 21:43:58 -0500

    Agregar clase DataAnalyzer con métodos para analizar, graficar, guardar y cargar datos

---
+++ src/data_analyzer.py
@@ -0,0 +1,25 @@
+class DataAnalyzer:
+    def __init__(self, data):
+        self.data = data
+
+    def analyze(self):
+        # analyze data
+        pass
+
+    def plot(self):
+        # plot data
+        pass
+
+    def save(self):
+        # save data
+        pass
+
+    def load(self):
+        # load data
+        pass
+
+    def run(self):
+        self.analyze()
+        self.plot()
+        self.save()
+        self.load()
